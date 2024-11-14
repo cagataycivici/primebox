17038375859a5c3f849360a8fbf320607063a41d
Cagatay Civici <cagatay.civici@gmail.com>
2024-11-15 00:45:26 +0300

    Add PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { providePrimeNgConfig } from 'primeng/api';
+import Aura from '@primeng/themes/aura';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    providePrimeNgConfig({ 
+      theme: {
+        preset: Aura,
+        options: {}
+      }
+    })
+  ]
 };
