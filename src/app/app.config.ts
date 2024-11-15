c6977dff05d11f2bdae4270452de14e08f6d8e9a
Cagatay Civici <cagatay.civici@gmail.com>
2024-11-15 15:36:04 +0300

    Update to beta.5 and add sample

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-import { providePrimeNgConfig } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    providePrimeNgConfig({ 
+    provideAnimationsAsync(),
+    providePrimeNG({ 
       theme: {
-        preset: Aura,
-        options: {}
+        preset: Aura
       }
     })
   ]
